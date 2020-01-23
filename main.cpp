ec8ad8636c133f4e2653e50e2bc5774fe7878a60
dionb112 <dion-b@hotmail.com>
2020-01-23 23:26:30 +0000

    Lecture 8 c++ thread first draft (issue hanging after first production dumps to buffer and p goes up by 1, but debugging and later in consumer p is back to 0 and we get an infinite spin...

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <thread>
+#include <iostream>
+#include <chrono>
+
+
+void producer(int buffer, int p, int c) {
+	int a[5] = { 10, 20, 30, 40, 50 };
+	bool run = true;
+	while (run) {
+		while (p < 5) {
+			std::cout << "ID:" << std::this_thread::get_id() << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+			while (!(p == c)) {
+				std::cout << "prod spinning" << std::endl;
+			}
+			buffer = a[p];
+			std::cout << "Number:" << a[p] << " dump to buffer" << std::endl;
+			p = p + 1;
+		}
+		std::cout << "prod done" << std::endl;
+		run = false;
+	}
+}
+
+void consumer(int buffer, int p, int c) {
+	int b[5];
+	bool run = true;
+	while (run) {
+		while (c < 5) {
+			std::cout << "ID:" << std::this_thread::get_id() << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			while ((p <= c)) {
+				std::cout << "cons spinning" << std::endl;
+			}
+			b[c] = buffer;
+			std::cout << buffer << " dump to b array" << std::endl;
+			c = c + 1;
+		}
+		std::cout << "consumption done" << std::endl;
+		run = false;
+	}
+}
+
+int main(void)
+{
+	int buffer, p = 0, c = 0;
+	std::thread producer(producer, buffer, p, c);
+	std::thread consumer(consumer, buffer, p, c);
+	producer.join();
+	consumer.join();
+	std::cin.get();
+	return 1;
+}
